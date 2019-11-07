c8eba9304ce9abe83b89328edf9313f8a6c4fb11
Jadie Adams <u0930409@utah.edu>
2019-11-07 16:35:13 -0700

    Cleaned and commented a little

--- js/AreaChart.js
+++ js/AreaChart.js
@@ -5,8 +5,6 @@ class AreaChart{
     constructor(data, histData) {
         this.data = data;
         this.histData = histData
-        console.log(histData)
-
         this.hist = {
             'width':  1200,
             'height' : 100
@@ -17,8 +15,6 @@ class AreaChart{
         }
 
         this.attributes = ["trigger", "aspect", "size", "elevation"];
-        this.ordinal = ["size", "elevation"];
-
         this.sortedLabels = {};
         this.setSortedLabels();
 
@@ -27,6 +23,7 @@ class AreaChart{
         this.createDropDown();
     };
 
+    // sets sorted label order
     setSortedLabels(){
         // get a valid year and month
         let year = Object.keys(this.data)[0];
@@ -39,15 +36,18 @@ class AreaChart{
         this.sortedLabels["elevation"] = Object.keys(this.data[year][month]["elevation"]);   
     }
 
+    // returns sorted list of category labels
     sortCategory(category){
+        // get a valid year and month
         let startYear = Object.keys(this.data)[0];
         let startMonth = Object.keys(this.data[startYear])[0];
         let labels = Object.keys(this.data[startYear][startMonth][category]);
+        // initialize dict for counts of labels
         let count_dict = {};
         for (let index = 0; index < labels.length; index++){
             count_dict[labels[index]] = 0;
         }
-        // set
+        // set counts of labels
         for (let year in this.data) {
             for (let month in this.data[year]) {
                 for (let index = 0; index < labels.length; index++){
@@ -55,26 +55,26 @@ class AreaChart{
                 }
             }
         }
-        //sort
+        //sort by counts of labels
         let sorted_labels = Object.keys(count_dict).sort(function(a, b) {
           return count_dict[b] - count_dict[a];
         })
         return sorted_labels;
-
     }
 
+    // draws histogram of all data
     createHistogram(){
         let that = this;
         // svg
         let histSVG = d3.select('#histogram').append('svg')
             .attr("width", this.hist.width)
             .attr("height", this.hist.height);
-        //border
+        // border
         histSVG.append("rect")
             .attr("width", this.hist.width)
             .attr("height", this.hist.height)
             .attr("class", "border")
-        //scale
+        // scale
         let xHistScale = d3.scaleLinear()
             .domain([0, this.histData.length])
             .range([0, this.hist.width]);
@@ -82,8 +82,9 @@ class AreaChart{
         let yHistScale = d3.scaleLinear()
             .domain([0,maxCount])
             .range([0, this.hist.height-10]);
+        // histogram bars
         let histBars = histSVG.append("g").attr('id', 'histBars');
-        let bars = histBars.selectAll("rect").data(this.histData);    //update
+        let bars = histBars.selectAll("rect").data(this.histData);
         let enterBars = bars.enter().append("rect");
         bars.exit().remove();
         bars = enterBars.merge(bars);
@@ -109,12 +110,10 @@ class AreaChart{
             .attr("width", this.area.width)
             .attr("height", this.area.height)
             .attr("class", "border");
-
         //define series
         let areaData = this.getAreaData(attribute);
         let columns = Object.keys(areaData[0]);
         let series = d3.stack().keys(columns.slice(1))(areaData); // don't include date
-
         // bar scales
         let xBarScale = d3.scaleLinear()
             .domain([0, areaData.length])
@@ -123,7 +122,6 @@ class AreaChart{
             .domain([0,1])
             .range([this.area.height,0]);
         let color = d3.scaleOrdinal(d3.schemeCategory10);
-
         // add bars
         let catGroups = areaSVG.selectAll("g").data(series).enter()
             .append("g")
@@ -137,17 +135,16 @@ class AreaChart{
             .attr("width", 10)
             .on('click', function(d, i) {
                 console.log(d);
-            });;
+            });
     }
 
+    // updates area chart with given attribute data
     updateAreaChart(attribute){
         let areaSVG = d3.select("#areaSVG")
-
         //define series
         let areaData = this.getAreaData(attribute);
         let columns = Object.keys(areaData[0]);
         let series = d3.stack().keys(columns.slice(1))(areaData); // don't include date
-
         // bar scales
         let xBarScale = d3.scaleLinear()
             .domain([0, areaData.length])
@@ -156,7 +153,6 @@ class AreaChart{
             .domain([0,1])
             .range([this.area.height,0]);
         let color = d3.scaleOrdinal(d3.schemeCategory10);
-
         // add bars
         let catGroups = areaSVG.selectAll("g").data(series);
         let catEnter = catGroups.enter().append("g").style("fill", d => color(d.key));
@@ -180,6 +176,7 @@ class AreaChart{
             .style("opacity", 1);
     }
 
+    // gets area chart data with specified category
     getAreaData(category){
         let areaData = [];
         let allZeros = true;
@@ -210,12 +207,10 @@ class AreaChart{
                 areaData.push(dict) ;     
             } 
         }
-        // console.log(areaData)
         return areaData;
     };
 
-
-
+    // TODO creates drop down
     createDropDown(){
         let that = this;
 
